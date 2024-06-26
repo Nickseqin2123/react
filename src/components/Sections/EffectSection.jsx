9d59ceac957968e1f206f393a34bfd46cc271d2b
Nikita <bletigri@gmail.com>
2024-06-26 21:29:28 +0300

    end project

--- src/components/Sections/EffectSection.jsx
+++ src/components/Sections/EffectSection.jsx
@@ -1,21 +1,36 @@
-import { useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import Button from '../Button/Button'
 import Modal from '../Modal/Modal'
+import useInput from '../Hooks/useinput'
 
 
 export default function EffectSection() {
+    const input = useInput()
     const [modal, setModal] = useState(false)
+    const [loading, setLoading] = useState(false)
+    const [users, setUsers] = useState([])
+
+    const fetchUsers = useCallback(
+        async () => {
+            setLoading(true)
+            const response = await fetch("https://jsonplaceholder.typicode.com/users")
+            const data = await response.json()
+            setUsers(data)
+            setLoading(false)
+        }, []
+    )
+
+    useEffect(() => {
+        fetchUsers()
+    }, [fetchUsers])
 
 
-    function openModal() {
-        setModal(true)
-    }
     return (
         <section>
             <h3>
                 Effects
             </h3>
-            <Button onClick={openModal}>Открыть информацию</Button>
+            <Button style={{ marginBottom: '1rem' }} onClick={() => setModal(true)}>Открыть информацию</Button>
             <Modal open={modal}>
                 <h3>Hello from modal</h3>
                 <p>
@@ -23,6 +38,23 @@ export default function EffectSection() {
                 </p>
                 <Button onClick={() => {setModal(false)}}>Закрыть окно</Button>
             </Modal>
+            {loading && <p>Loading...</p>}
+
+            {!loading && (
+                <>
+                    <input type="text" className='control' {...input}/>
+                    <h6>{input.value}</h6>
+                    <ul>
+                        {users.filter((user) => 
+                            user.name.toLowerCase().includes(
+                                input.value.toLowerCase()
+                            )
+                        ).map((user) => 
+                            <li key={user.id}>{user.name}</li>
+                        )}
+                    </ul>
+                </>
+            )}
         </section>
     )
 }
